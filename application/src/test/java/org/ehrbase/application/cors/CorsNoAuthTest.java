e0213add4bf965257cf865bf99e6ec2a206a0702
Renaud Subiger <renaud@subiger.com>
2021-11-29 19:30:04 +0100

    Fixes CORS issue when security is enabled (#619)

---
+++ application/src/test/java/org/ehrbase/application/cors/CorsNoAuthTest.java
@@ -0,0 +1,26 @@
+package org.ehrbase.application.cors;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class CorsNoAuthTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testCors() throws Exception {
+        mockMvc.perform(options("/rest/openehr/v1/definition/template/adl1.4"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+}
